42c8b3e876787d5a8ebccb6cc010fae70d6640ae
Marina Morais <marina.morais.lacerda@ccc.ufcg.edu.br>
2025-08-11 20:02:31 -0300

    Update Conquistas.hs

--- src/Funcionalidades/Conquistas.hs
+++ src/Funcionalidades/Conquistas.hs
@@ -1,35 +1,64 @@
-module Funcionalidades.Conquistas (
-  conquistasDisponiveis,
-  getConquistasUsuario 
-) where
+module Funcionalidades.Conquistas
+  ( conquistasDisponiveis
+  , getConquistasUsuario
+  ) where
 
-import Types.Usuario
-import Types.Scrobble
-import Types.Musica
+import qualified Types.Scrobble as S
+import qualified Types.Musica   as M
+import Data.List (group, sort)
 
 conquistasDisponiveis :: [String]
-conquistasDisponiveis = 
-    [  "Primeiro Scrobble!", 
-       "10 músicas ouvidas", 
-       "Super Fã!", 
-       "100 minutos escutados"  
-    ]
+conquistasDisponiveis =
+  [ "Primeiro Scrobble!"
+  , "10 músicas ouvidas"
+  , "100 minutos escutados"
+  ]
 
-getConquistasUsuario :: [Scrobble] -> [String]
+getConquistasUsuario :: [S.Scrobble] -> [String]
 getConquistasUsuario scrobblesUsuario =
   let
+    totalScrobbles :: Int
     totalScrobbles = length scrobblesUsuario
-    totalMinutosDouble = sum (map (fromIntegral . duracao . musica) scrobblesUsuario) / 60
-    totalMinutos = floor totalMinutosDouble
-    novasConquistas = [ c | (c, cond) <- regras totalScrobbles totalMinutos, cond ]
-  in novasConquistas
 
+    totalMinutos :: Int
+    totalMinutos = sum (map (M.duracao . S.musica) scrobblesUsuario) `div` 60
 
+    conquistasFixas :: [String]
+    conquistasFixas =
+      [ c | (c, cond) <- regras totalScrobbles totalMinutos, cond ]
+
+    artistas :: [String]
+    artistas = map (M.artista . S.musica) scrobblesUsuario
+
+    superFaArtistas :: [String]
+    superFaArtistas =
+      [ "Super Fã de " ++ a
+      | (a, n) <- groupCount artistas
+      , n >= 50
+      ]
+
+    musicasChave :: [(String, String)]
+    musicasChave =
+      [ (M.titulo m, M.artista m)
+      | s <- scrobblesUsuario
+      , let m = S.musica s
+      ]
+
+    superFaMusicas :: [String]
+    superFaMusicas =
+      [ "Super Fã da música " ++ t ++ " - " ++ a
+      | ((t,a), n) <- groupCount musicasChave
+      , n >= 50
+      ]
+  in
+    conquistasFixas ++ superFaArtistas ++ superFaMusicas
 
 regras :: Int -> Int -> [(String, Bool)]
 regras totalScrobbles totalMinutos =
   [ ("Primeiro Scrobble!", totalScrobbles >= 1)
   , ("10 músicas ouvidas", totalScrobbles >= 10)
-  , ("Super Fã!", totalScrobbles >= 50)
   , ("100 minutos escutados", totalMinutos >= 100)
   ]
+
+groupCount :: Ord a => [a] -> [(a, Int)]
+groupCount xs = map (\g -> (head g, length g)) . group . sort $ xs
